9e342e985b22467cae1e96237849c86d3fe75e2a
Leandro Cunha <leandroscunha@gmail.com>
2018-09-15 14:40:44 -0300

    fix Batch key

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -6,7 +6,7 @@ const Offers = props => (
     <h3>{props.name}</h3>
     <p>{props.description}</p>
     {props.batches.map(batch => (
-      <Batch key={batch.key} {...batch} />
+      <Batch key={batch.id} {...batch} />
     ))}
   </div>
 );
