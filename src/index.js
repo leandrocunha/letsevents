e04a2049057bb9cf5d4cc422fe67f0e70591e538
Leandro Cunha <leandroscunha@gmail.com>
2018-09-15 10:54:58 -0300

    eslint and airbnb js styleguide

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hi, Lets!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
